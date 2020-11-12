37e6714c03932f424171b7333e9a98035e000664
IVBecy <57770911+IVBecy@users.noreply.github.com>
2020-11-12 18:25:28 +0000

    Reverting to .php in public script search instead of /{filename}

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,15 +7,15 @@ $(document).ready(() => {
     const RenderDropDown = () => {
       return (
         <div className="dropdown_menu" id="dropdown_settings" style={{ visibility: "hidden", width:"200px" }}>
-          <form method="POST" action="../public/profile-src">
+          <form method="POST" action="../public/profile-src.php">
             <input className="search-usrs" type="search" name="src_name" placeholder="Search" />
           </form>
           <hr/>
-          <span><a href="../public/profile">Your Profile</a></span>
-          <span><a href="../public/feed">Your Feed</a></span>
+          <span><a href="../public/profile.php">Your Profile</a></span>
+          <span><a href="../public/feed.php">Your Feed</a></span>
           <span id="new_project">New project</span>
           <hr />
-          <span><a href="../public/settings">Settings</a></span>
+          <span><a href="../public/settings.php">Settings</a></span>
           <span>Docs</span>
           <span><a href="../private/logout.php" style={{color:"red"}}>Sign out</a></span>
         </div> 
