2fc17c31e3fbe1319af7122c73ef44767357898f
IVBecy <57770911+IVBecy@users.noreply.github.com>
2020-11-22 17:57:18 +0000

    Added name lookup file

---
+++ private/name-lookup.php
@@ -0,0 +1,24 @@
+<?php
+#Turn off all notices
+error_reporting(E_ALL & ~E_NOTICE);
+#Adding the script that connects to the DB
+include("./connect.php");
+include("./vars.php");
+# Get the str parsed in at JS
+$str = mysqli_real_escape_string($connection,e($_GET["str"]));
+$output = "";
+# Get all the usernames that start with the str
+$q = "SELECT `uname` FROM `users` WHERE `uname` LIKE '$str%'";
+$names = mysqli_query($connection,$q);
+$names = mysqli_fetch_row($names);
+# Output names from query
+if ($names != ""){
+  foreach($names as $n){
+    $output .= "$n";
+  }
+  echo "<span><a href='./$output'>$output</a></span>";
+}else{
+  $output =  "No match"; 
+  echo $output;
+}
+?>
