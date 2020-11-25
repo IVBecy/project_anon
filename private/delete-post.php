c7bdc61b7757684e7d0a593add638ebbe3cfb639
IVBecy <57770911+IVBecy@users.noreply.github.com>
2020-11-25 19:27:14 +0000

    Better post deleteing script

--- private/delete-post.php
+++ private/delete-post.php
@@ -1,31 +1,18 @@
 <?php 
 #Session (start and vars)
 session_start();
-$uname = $_SESSION["uname"];
 # Turn off all notices
 error_reporting(E_ALL & ~E_NOTICE);
 #Adding the script that connects to the DB
 include("./connect.php");
 #getting some vars
 include("./vars.php");
-#Getting the projects from the database
-$collection = [];
-$p_query = "SELECT `title`,`report` FROM `posts` WHERE `name_id` = '$id'";
-$projects = mysqli_query($connection,$p_query);
 if(hash_equals($_SESSION["csrf-token"], $_POST["csrftoken"])){
-  #Appending all the projects to one array
-  while ($row = mysqli_fetch_assoc($projects)) {
-    array_push($collection,$row);
-  };
   #Deletion cookie
-  $delete_item = e($_COOKIE["ToBeDeleted"]);
+  $delete_item = mysqli_real_escape_string($connection,e($_COOKIE["ToBeDeleted"]));
   #Delete the post from the DB
-  foreach($collection as $k) {
-  if ($k["title"] == $delete_item){
-    $delete_query = "DELETE FROM `posts` WHERE `name_id` = '$id' AND `title` = '$delete_item'";
-    $connection->query($delete_query);
-    }
-  }
+  $delete_query = "DELETE FROM `posts` WHERE `name_id` = '$id' AND `title` = '$delete_item'";
+  if ($connection->query($delete_query) === true){}else{$connection->error;};
   #Create new csrf token
   createCSRF();
   #Delete cookie 
